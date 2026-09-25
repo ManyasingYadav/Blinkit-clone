69cac11460f73847b05f70d3b3d96ad9700823e1
manyasing yadav <manyayadav2020@gmail.com>
2026-09-25 15:04:33 +0530

    inttial commit -Blinkit react frontend

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Blinkit</h1>
+    </div>
+    
+  )
+}
+
+export default App
